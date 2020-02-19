2f2be9766a1e14112288285106993be575bf43de
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-02-19 17:31:44 +0800

    Update list.php

--- products/list.php
+++ products/list.php
@@ -1,1 +1,31 @@
-
+<html>
+<body>
+<?php 
+  $json = file_get_contents('http://rdapi.herokuapp.com/product/read.php');
+  $data = json_decode($json, true);
+  $list = $data['records'];
+  ?>
+  <table border = "1">
+   <thead>
+    <tr>
+      <th>Name</th>
+      <th>Price</th>
+      <th>Description</th>
+      <th>Category</th>
+    <tr>
+   </thead>
+   <tbody>
+   <?php
+     foreach( $list as $value )
+    {?>
+     <tr>
+        <td><?php echo $value['name'];?></td>
+        <td><?php echo $value['price'];?></td>
+        <td><?php echo $value['description'];?></td>
+        <td><?php echo $value['category'];?></td>
+    </tr>
+   </tbody>
+      
+  </table>
+</body>
+</html>
