9188b32c9b9f744d78d2e1e3002a9824e667c945
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-02-26 00:20:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<?php
-switch ($_GET['page']) {
-    case 'product':
-        include("products/list.php");
-        break;
-    case 'add':
-        create
-        break;
-   // case 'view':
-     //   view
-//break;
-    default:
-         include("products/list.php");
-?>
+
