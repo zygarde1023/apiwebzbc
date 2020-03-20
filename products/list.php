6b3defbb8f724892b94851c91c33da7ecb4d45da
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-03-20 13:22:11 +0800

    Update list.php

--- products/list.php
+++ products/list.php
@@ -1,5 +1,6 @@
 <html>
 <body>
+<a style = "align: right;" href = "logout.php">Logout</a>
 <?php 
   $json = file_get_contents('http://rdapi.herokuapp.com/product/read.php');
   $data = json_decode($json, true);
