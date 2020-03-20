be8299c1128eaa9654bd15b46541c92d9cef551f
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-03-20 15:07:39 +0800

    Update main.php

--- main.php
+++ main.php
@@ -1,4 +1,4 @@
-<div style="background-color: #45a29e;
+<div style="background-color: #ffffff;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   margin: 50px 0px 50px 0px;  width: 50%; padding: 5vh 0vh 5vh 0vh;">
   <img src="<?php echo $_SESSION["user_image"] ?>" alt="profile pic" style="width:40%; margin: 0 auto;">
@@ -10,4 +10,4 @@
 <h1>Welcome, <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?>! </h1>
 
 <br><br>
-<a href="logout.php"><div class="button-custom" style="background-color: red;">Logout</div></a></h3>
+<a href="logout.php"><i class="mdi mdi-logout"><div class="btn-danger btn-rounded" >Logout</div></i></a>
