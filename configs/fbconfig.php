b5251324dcda69f8c053eed2d5508058fa15ce8a
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-03-20 14:32:14 +0800

    Add files via upload

---
+++ configs/fbconfig.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+if (!session_id())
+{
+    session_start();
+}
+
+$facebook = new \Facebook\Facebook([
+  'app_id'      => '2704847169627884',
+  'app_secret'     => 'e71a05412c40a782a96e71782a4280d8',
+  'default_graph_version'  => 'v2.10'
+]);
+//weblessonzzz
+?>
---
+++ configs/gglconfig.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'vendor/autoload.php';
+$google_client = new Google_Client();
+$google_client->setClientId('442882868612-fsac45rrngl3gmrp6g9gkoph6m198akq.apps.googleusercontent.com');
+$google_client->setClientSecret('i4Jya531Y-z3BFTJA9tCIhFV');
+$google_client->setRedirectUri('https://apiwebzbc.herokuapp.com/index.php?view=list');
+$google_client->addScope('email');
+$google_client->addScope('profile');
+
+session_start();
+//weblessonzzz
+?>
