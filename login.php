3e0e22cf8dd2062aa2f0770f30c944e2e77311a8
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-03-20 13:58:30 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,135 +1,105 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="google-signin-scope" content="profile email">
-<meta name="google-signin-client_id" content="744925265887-pnfor9hk0nntd64kj9akf5o0k55jphuq.apps.googleusercontent.com">
-<script src="https://apis.google.com/js/platform.js" async defer></script>
-<title>Exam</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="login-form">
-        <h2 class="text-center">Sign in</h2>
-		<br>
-        <div class="text-center social-btn">
-            <div id="fb-root"></div>
-			<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0&appId=2704847169627884&autoLogAppEvents=1">
-			</script>
-			<?php
-			
-			require 'vendor/autoload.php';
-			
-			$fb = new Facebook\Facebook([
-			 'app_id' => '2704847169627884',
-			 'app_secret' => 'e71a05412c40a782a96e71782a4280d8',
-			 'default_graph_version' => 'v2.10',
-			]);
-			$helper = $fb->getRedirectLoginHelper();
-			$permissions = ['email']; // optional
-			
-			try {
-				
-			if (isset($_SESSION['facebook_access_token'])) {
-				header('Location: https://apiwebzbc.herokuapp.com/index.php?view=list');
-			
-			} else {
-			  $accessToken = $helper->getAccessToken();
-			  $oAuth2Client = $fb->getOAuth2Client();
-			}
-			} catch(Facebook\Exceptions\facebookResponseException $e) {
-			// When Graph returns an error
-			echo 'Graph returned an error: ' . $e->getMessage();
-			  exit;
-			} catch(Facebook\Exceptions\FacebookSDKException $e) {
-			// When validation fails or other local issues
-			echo 'Facebook SDK returned an error: ' . $e->getMessage();
-			  exit;
-			}
-			if (isset($accessToken)) {
-				header('Location: : ');
-				
-			if (isset($_SESSION['facebook_access_token'])) {
-				
-			$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-			} else {
-			// getting short-lived access token
-			$_SESSION['facebook_access_token'] = (string) $accessToken;
-			  // OAuth 2.0 client handler
-			$oAuth2Client = $fb->getOAuth2Client();
-			// Exchanges a short-lived access token for a long-lived one
-			$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-			$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-			// setting default access token to be used in script
-			$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-			}
-			// redirect the user to the profile page if it has "code" GET variable
-			if (isset($_GET['code'])) {
-			header('Location: https://apiwebzbc.herokuapp.com/index.php?view=list');
-			}
-			// getting basic info about user
-			try {
-			header('Location: https://apiwebzbc.herokuapp.com/index.php?view=list');
-			$profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
-			$requestPicture = $fb->get('/me/picture?redirect=false&height=200'); //getting user picture
-			$picture = $requestPicture->getGraphUser();
-			$profile = $profile_request->getGraphUser();
-			$fbid = $profile->getProperty('id');           // To Get Facebook ID
-			$fbfullname = $profile->getProperty('name');   // To Get Facebook full name
-			$fbemail = $profile->getProperty('email');    //  To Get Facebook email
-			$fbpic = "<img src='".$picture['url']."' class='img-rounded'/>";
-			# save the user nformation in session variable
-			$_SESSION['fb_id'] = $fbid.'</br>';
-			$_SESSION['fb_name'] = $fbfullname.'</br>';
-			$_SESSION['fb_email'] = $fbemail.'</br>';
-			$_SESSION['fb_pic'] = $fbpic.'</br>';
-			} catch(Facebook\Exceptions\FacebookResponseException $e) {
-			// When Graph returns an error
-			echo 'Graph returned an error: ' . $e->getMessage();
-			// redirecting user back to app login page
-			header("Location: ./");
-			exit;
-			} catch(Facebook\Exceptions\FacebookSDKException $e) {
-			// When validation fails or other local issues
-			echo 'Facebook SDK returned an error: ' . $e->getMessage();
-			exit;
-			}
-			} else {
-			// replace your website URL same as added in the developers.Facebook.com/apps e.g. if you used http instead of https and you used            
-			$loginUrl = $helper->getLoginUrl('https://apiwebzbc.herokuapp.com/index.php?view=list', $permissions);
-			echo '<a href="' . $loginUrl . '"><div class="fb-login-button" data-width="" data-size="large" data-button-type="login_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="true"></div></a>';
-			}
-			?>
-			<br>
-			<br>
-			<div id="my-signin2" data-onsuccess="onSuccess" data-redirecturi="https://apiwebzbc.herokuapp.com/index.php?view=list"></div>
-			<script>
-    function onSuccess(googleUser) {
-        window.location="/index.php?view=list";
-    }
-    function onFailure(error) {
-      console.log(error);
-    }
-    function renderButton() {
-      gapi.signin2.render('my-signin2', {
-        'scope': 'profile email',
-        'width': 230,
-        'height': 40,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onSuccess,
-        'onfailure': onFailure
-      });
-    }
-  </script>
+<?php
 
-  <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
-        </div>
-</div>
-</body>
-</html>         
+//index.php
+
+include('configfb.php');
+
+$facebook_output = '';
+
+$facebook_helper = $facebook->getRedirectLoginHelper();
+
+if(isset($_GET['code']))
+{
+ if(isset($_SESSION['access_token']))
+ {
+  $access_token = $_SESSION['access_token'];
+ }
+ else
+ {
+  $access_token = $facebook_helper->getAccessToken();
+
+  $_SESSION['access_token'] = $access_token;
+
+  $facebook->setDefaultAccessToken($_SESSION['access_token']);
+ }
+
+ $_SESSION['user_id'] = '';
+ $_SESSION['user_name'] = '';
+ $_SESSION['user_email_address'] = '';
+ $_SESSION['user_image'] = '';
+
+ $graph_response = $facebook->get("/me?fields=name,email", $access_token);
+
+ $facebook_user_info = $graph_response->getGraphUser();
+
+ if(!empty($facebook_user_info['id']))
+ {
+  $_SESSION['user_image'] = 'http://graph.facebook.com/'.$facebook_user_info['id'].'/picture';
+ }
+
+ if(!empty($facebook_user_info['name']))
+ {
+  $_SESSION['user_name'] = $facebook_user_info['name'];
+ }
+
+ if(!empty($facebook_user_info['email']))
+ {
+  $_SESSION['user_email_address'] = $facebook_user_info['email'];
+ }
+ 
+}
+else
+{
+ // Get login url
+    $facebook_permissions = ['email']; // Optional permissions
+
+    $facebook_login_url = $facebook_helper->getLoginUrl('https://apiwebzbc.herokuapp.com/index.php?view=list', $facebook_permissions);
+    
+    // Render Facebook login button
+    $facebook_login_url = '<div align="center" ><a href="'.$facebook_login_url.'"><img width = "100" height = "100" src="image/fb.jpg" /></a></div>';
+}
+
+
+
+?>
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Login With:</title>
+  <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="vendors/base/vendor.bundle.base.css">
+  <link rel="stylesheet" href="css/style.css">
+  <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+  
+ </head>
+ <body>
+  <div class="container">
+   <br />
+   <h2 align="center">Login With:</h2>
+   <br />
+   <div class="panel panel-default">
+    <?php
+    if(isset($facebook_login_url))
+    {
+     //echo $facebook_login_url;
+    }
+    else
+    {
+     echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
+     echo '<img src="'.$_SESSION["user_image"].'" class="img-responsive img-circle img-thumbnail" />';
+     echo '<h3><b>Name :</b> '.$_SESSION['user_name'].'</h3>';
+     echo '<h3><b>Email :</b> '.$_SESSION['user_email_address'].'</h3>';
+     echo '<h3><a href="logout.php">Logout</h3></div>';
+    }
+    ?>
+   </div>
+  </div>
+  <script src="vendors/base/vendor.bundle.base.js"></script>
+  <script src="js/off-canvas.js"></script>
+  <script src="js/hoverable-collapse.js"></script>
+  <script src="js/template.js"></script>
+ </body>
+</html>
